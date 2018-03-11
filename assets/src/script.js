6a8c635d5fea95b985b791917e0dea09bf2dcd21
Trevor Johnson <johnsontrevor55@gmail.com>
2018-03-11 14:28:19 -0700

    started layout

---
+++ assets/src/script.js
@@ -0,0 +1,13 @@
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyD1Bp9JkiAS1J1JutbizPWF7_ofCMWsZh8",
+  authDomain: "rps-multiplayer-80838.firebaseapp.com",
+  databaseURL: "https://rps-multiplayer-80838.firebaseio.com",
+  projectId: "rps-multiplayer-80838",
+  storageBucket: "rps-multiplayer-80838.appspot.com",
+  messagingSenderId: "177104249799"
+};
+firebase.initializeApp(config);
+
+var database = firebase.database();
+
