3c3e65c5dd00caba89046a2dcc8fb43fff475361
Trevor Johnson <johnsontrevor55@gmail.com>
2018-03-13 18:44:29 -0700

    can save users to db

--- assets/src/script.js
+++ assets/src/script.js
@@ -11,3 +11,69 @@ firebase.initializeApp(config);
 
 var database = firebase.database();
 
+var p1Name;
+var p2Name;
+var p1Choice;
+var p2Choice;
+var numberOfUsers = 0;
+
+//used for the click event on submit button to get user name
+var setName = function() {
+
+  //tracks how many players we have. Either 1 or 2
+  numberOfUsers++;
+
+  //sets player 1
+  if (numberOfUsers === 1) {
+
+    //gets player 1 input
+    p1Name = document.getElementById('user-name').value.trim();
+    console.log(p1Name)
+    console.log(numberOfUsers)
+
+    //stores player 1 in the db
+    database.ref('players').push({
+      p1Name: p1Name
+    });
+  }
+
+  //sets player 2
+  if (numberOfUsers === 2) {
+
+    //gets player 2 input
+    p1Name = document.getElementById('user-name').value.trim();
+    console.log(p2Name)
+    console.log(numberOfUsers)
+
+    //stores player 2 in the db
+    database.ref('players').push({
+      p2Name: p2Name
+    });
+
+  }
+  //clears user input
+  document.getElementById('user-name').value = '';
+}
+
+//sets the names of each player
+document.getElementById('submit-name').onclick = function (e) { 
+  e.preventDefault(); 
+  setName(); 
+}
+
+database.ref().on("value", function(snapshot) {
+
+  //sets player 1 name
+  document.getElementById('p1').innerText = snapshot.child("players/player1/name").val();
+
+  //sets player 2 name
+  document.getElementById('p2').innerText = snapshot.child("players/player2/name").val();
+
+}, function(error) {
+
+});
+
+// database.ref().once("value")
+//   .then((snapshot) => {
+//     document.getElementById('p1').innerText = snapshot.child("players/player1/name").val();
+//   });
